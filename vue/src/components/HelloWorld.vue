0568694b2d158df0a40ac4bee164ae71d890fbe1
wo-000 <2535834917@qq.com>
2019-03-10 10:46:09 +0800

    vue项目学习

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+
+   {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      text: '123,456'
+    }
+  },
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
