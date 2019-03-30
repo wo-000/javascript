73a5904846b49e10cfa77f5ccd08ddab65b122e6
wo-000 <2535834917@qq.com>
2019-03-30 14:37:51 +0800

    页面头部

--- go-where/src/components/Header.vue
+++ go-where/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-left">bbb</div>
-    <div class="header-input"></div>
-    <div class="header-right"></div>
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市景点</div>
+    <div class="header-right">城市</div>
   </div>
 </template>
 
@@ -20,5 +20,25 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
   .header
-    height: .86rem;
+    font-size:0.24rem;
+    line-height: .86rem;
+    display: flex;
+    background : #00bcd4;
+    color: #fff;
+    .header-left
+      width : .64rem;
+      float : left;
+    .header-input
+      flex:1;
+      height: 0.64rem;
+      margin-top .12rem;
+      border-radius: .1rem;
+      background : #fff;
+      color: #ccc;
+      line-height: .64rem;
+      padding-left: .12rem;
+    .header-right
+      width: 1.24rem;
+      float : right;
+      text-align center;
 </style>
