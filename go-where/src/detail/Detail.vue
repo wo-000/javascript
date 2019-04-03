ba95bca884653d610fd5080b222ed7bbba391130
wo-000 <2535834917@qq.com>
2019-04-03 12:48:38 +0800

    详情页请求数据，页面滚动定位问题，页面keep-alive缓存问题

--- go-where/src/detail/Detail.vue
+++ go-where/src/detail/Detail.vue
@@ -1,14 +1,50 @@
 <template>
-    <div>
-        <Banner></Banner>
+    <div class="content">
+        <Banner :sightName="sightName" :bannerImg="bannerImg" :gallaryImgs="gallaryImgs"></Banner>
+        <DetailHeader></DetailHeader>
+        <DetailList :list="list"></DetailList>
     </div>
 </template>
 <script>
+import Aaxios from 'axios'
 import Banner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
     name:'Detail',
+    data(){
+        return{
+            sightName:'',
+            bannerImg:'',
+            gallaryImgs:[],
+            list:[],
+            
+        }
+    },
     components:{
         Banner,
+        DetailHeader,
+        DetailList
+    },
+    methods:{
+        getDetailInfo(){
+            // Aaxios.get('/api/detail.json?id=' + this.$route.params.id)
+            Aaxios.get('/api/detail.json',{
+                params:{
+                    id:this.$route.params.id
+                }
+            }).then(this.handleGetDataSuc)
+        },
+        handleGetDataSuc(res){
+            this.sightName=res.data.data.sightName;
+            this.bannerImg=res.data.data.bannerImg;
+            this.gallaryImgs=res.data.data.gallaryImgs;
+            this.list=res.data.data.categoryList;
+            console.log(res.data)
+        }
+    },
+    mounted() {
+        this.getDetailInfo()
     }
 }
 </script>
--- go-where/src/detail/components/Banner.vue
+++ go-where/src/detail/components/Banner.vue
@@ -1,27 +1,31 @@
 <template>
     <div>
         <div class="banner" @click="handleBannerClick">
-            <img class="banner-img" src="" alt="">
+            <img class="banner-img" :src="this.bannerImg" alt="">
             <div class="banner-info">
-                <div class="banner-title">aaaaaaaaa</div>
+                <div class="banner-title">{{this.sightName}}</div>
                 <div class="banner-number">
                     <span class="iconfont banner-icon">&#xe632;</span>
-                    39
+                    {{this.gallaryImgs.length}}
                 </div>
             </div>
         </div>
-        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+        <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
     </div>
     
 </template>
 <script>
 import CommonGallary from 'common/gallary/Gallary'
 export default {
-    name:'Detail',
+    name:'DetailBanner',
+    props:{
+        sightName:String,
+        bannerImg:String,
+        gallaryImgs:Array
+    },
     data(){
         return{
             showGallary:false,
-            imgs:['http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg','http://www.pptbz.com/pptpic/UploadFiles_6909/201203/2012031220134655.jpg']
         }
     },
     components:{
