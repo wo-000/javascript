1558c6fbce6ed7a212d0ecfcd6e62c21ed9b5c91
wo-000 <2535834917@qq.com>
2019-04-03 15:14:45 +0800

    fasrClick

--- go-where/src/App.vue
+++ go-where/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
 
-    <keep-alive exclude="Detail">
+    <keep-alive exclude="Detail">  
+      <!-- keep-alive  缓存问题  "exclude"特定某个组件不进行缓存用以多次调用数据  -->
       <router-view/>
     </keep-alive>
   </div>
--- go-where/src/main.js
+++ go-where/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'babel-polyfill'
 import Axios from 'axios'
 
 import store from './store'
