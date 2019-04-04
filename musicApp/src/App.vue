d80c0460044162f9511253f9c6e7a8329b6cc643
wo-000 <2535834917@qq.com>
2019-04-04 12:31:43 +0800

    vue音乐项目 and axios跨域获取音乐数据

---
+++ musicApp/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view>
+
+    </router-view>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  name: 'App',
+  components:{
+    MHeader,
+    Tab
+  },
+  created() {  
+    var url = '/api/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?_=1554349143478&g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1'
+    this.$axios.get(url).then(res=>{
+      console.log(res)
+    }).catch(error=>{
+      console.log(error)
+    })
+  },  
+  methods: {  
+  
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ musicApp/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from "axios"
+import 'babel-polyfill'
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+
+Vue.prototype.$axios=axios
+
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
